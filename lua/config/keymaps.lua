cc2c4f9c7ca89bdf0e56bfd4ac613876e72f38da
anotherdev <joseluismanco37@gmail.com>
2024-10-05 02:49:11 -0500

    feat: add conform

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,4 @@
-
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
-
-map('i', 'jk', '<Esc>', opts) -- Salir del modo insert con 'jk'
-
+map("i", "jk", "<Esc>", opts) -- Salir del modo insert con 'jk'
